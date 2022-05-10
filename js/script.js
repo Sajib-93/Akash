9ffe0b69c15152defac2812bfb71ac35f5989a12
Sajib-93 <msbsajib93@gmail.com>
2022-05-10 09:35:42 +0600

    Steel Building

--- js/script.js
+++ js/script.js
@@ -12,8 +12,31 @@ $(window).scroll(function(){
     }
 })
 
+$(window).scroll(function(){
+    if($(window).scrollTop() > 300){
+        $(".menu2").addClass("menufixed2")
+    }else{
+        $(".menu2").removeClass("menufixed2")
+    }
+
+})
+
+
 $(".common-button").click(function(){
     $("html,body").animate({
         scrollTop: 0
     }, 100)
 })
+
+AOS.init({
+    offset: 120, 
+    delay: 0, 
+    duration: 1000, 
+});
+
+$(".banner-main").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+})
+
